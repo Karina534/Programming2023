536d081e9ac0135e616cb753bb091682ab9fe49a
Karina534 <smirnova7.04@mail.ru>
2023-12-11 16:16:28 +0300

    fixed

--- tests/lab1/test_calculator.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.lab1 import calculator
-from tkinter import messagebox
-def test_calculat_sqrt():
-    assert calculator.sqrt(4) == 2
-    #with ValueError as error:
-        #assert messagebox.showinfo('Ты лох', "На ноль делить нельзя!")
